cb6309aea8d9c0f49534279ba18c9577597bcc5e
josephine robard <jorobard4@gmail.com>
2022-12-13 20:53:34 +0100

    new commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @user_question = params[:question]
+    if @user_question.end_with?('?')
+      @coach_answer = 'Silly question, get dressed and go to work!'
+    elsif @user_question == "I'm going to work"
+      @coach_answer = 'Great'
+    else
+      @coach_answer = 'I don t care, get dressed and go to work!'
+    end
+  end
+end
